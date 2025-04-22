4b3fbc97f39f555ba4865b17c2f6b51c0d88aceb
Soyoung <bayleaf07@naver.com>
2025-04-23 00:35:42 +0900

    feat: constructor

--- Class_Prac_1.java
+++ Class_Prac_1.java
@@ -1,15 +1,30 @@
 // Class Practice
 
-class Myhandbag {
+class Handbag {
+    // 브랜드 이름
     String brandName;
+    // 가격
     int price;
+    // 가방 이름
     String bagName;
+    // 손잡이 여부
     boolean hasHandle;
+
+    public Handbag(){
+        brandName = "";
+        price = 0;
+        bagName = "";
+        hasHandle = false;
+    }
 }
 
 public class Class_Prac_1 {
     public static void main(String[] args) {
-        Myhandbag myBag = new Myhandbag();
+        // 빈 파라미터의 생성자를 사용하여 객체 생성
+        // 생성자는 무조건, 클래스와 이름이 똑같아야 한다.
+        Handbag myBag = new Handbag();
+
+        // 객체의 속성 값 설정
         myBag.brandName = "Pink";
         myBag.price = 2000;
         myBag.bagName = "Marmont";
